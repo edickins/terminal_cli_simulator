78dac55876be4c7143b75aabafef4ef09dd31ee0
edickins <bleepbloop@bleepbloop.net>
2020-11-22 22:52:45 +0000

    added comments to formatter

--- src/app/formatters/textFormatter.js
+++ src/app/formatters/textFormatter.js
@@ -1,6 +1,17 @@
+/*
+Text formatter formats text on any data object
+that has a .txt String property.
+formatText method is invoked using Function.prototype.call
+to scope to that data Object
+*/
 export default class TextFormatter {
   constructor() {}
 
+  /*
+  format text using a specific element tag
+  add classes supplied in an Array
+  add .txt content and return that as a String
+  */
   formatText(configObj) {
     let classes = configObj.classes;
     let returnStr = "<" + configObj.element;
