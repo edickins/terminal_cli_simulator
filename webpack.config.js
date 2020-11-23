94bba09ed890a9d151e2ddbd28131ddfda992c10
Ed Dickins <ed.dickins@bleepbloop.net>
2020-11-23 14:29:56 +0000

    some changes to how elements in DisplayPanel are selected on construction

--- webpack.config.js
+++ webpack.config.js
@@ -1,11 +1,13 @@
 const path = require("path");
+const HtmlWebpackPlugin = require("html-webpack-plugin");
 
 module.exports = {
   entry: "./src/index.js",
   output: {
-    filename: "main.[contentHash].js",
+    filename: "main.[contenthash].js",
     path: path.resolve(__dirname, "dist"),
   },
+  devtool: "inline-source-map",
   module: {
     rules: [
       {
@@ -14,4 +16,9 @@ module.exports = {
       },
     ],
   },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: "./src/template.html",
+    }),
+  ],
 };
